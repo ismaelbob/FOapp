dbeee7e0beae16f92f061faeb345687188738f58
Ismael Jancko <bobismaeljg@gmail.com>
2023-04-04 16:42:20 -0400

    Se maqueto la vista de Tarjeta, sus secciones

--- tarjeta.php
+++ tarjeta.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Tarjeta | FO</title>
     <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/secciones.css">
 </head>
 <body>
     <header>
@@ -28,14 +29,35 @@
             </ul>
         </nav>
     </header>
-    <div>
-        Datos 
-    </div>
-    <div>
-        Tarjeta
-    </div>
-    <div>
-        Menu contextual 
+    <div class="container">
+        <div class="pnl_datos">
+            <h4>Datos</h4>
+            <form action="">
+                <input type="text" name="txtcodigo" id="txtcodigo">
+                <input type="button" value="Buscar">
+            </form>
+            <div>
+                <p>
+                    <span>Nombre:</span><br/>
+                    <span>José Pérez</span>
+                </p>
+                <p>
+                    <span>Edad:</span><br/>
+                    <span>12 años</span>
+                </p>
+            </div>
+        </div>
+        <div class="pnl_tarjeta">
+            <h4>Mi Tarjeta</h4>
+        </div>
+        <div class="pnl_menu">
+            <h4>Acciones</h4>
+            <button>Aporte</button>
+            <button>Multa</button>
+        </div>
+        <div class="pnl_lista">
+            Lista de recibos
+        </div>
     </div>
 </body>
 </html>
