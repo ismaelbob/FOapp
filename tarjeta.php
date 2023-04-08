c6d75179b64adee88408eb356dcb90d36f4bd98b
Ismael Jancko <bobismaeljg@gmail.com>
2023-04-08 14:22:18 -0400

    Se avanzo en la vista Tarjeta, se trabajo los estilos, aun falta terminar

--- tarjeta.php
+++ tarjeta.php
@@ -30,33 +30,35 @@
         </nav>
     </header>
     <div class="container">
-        <div class="pnl_datos">
-            <h4>Datos</h4>
-            <form action="">
-                <input type="text" name="txtcodigo" id="txtcodigo">
-                <input type="button" value="Buscar">
-            </form>
-            <div>
-                <p>
-                    <span>Nombre:</span><br/>
-                    <span>José Pérez</span>
-                </p>
-                <p>
-                    <span>Edad:</span><br/>
-                    <span>12 años</span>
-                </p>
+        <div class="pnl_principal">
+            <div class="pnl_datos">
+                <h4>Datos</h4>
+                <form action="">
+                    <input type="text" name="txtcodigo" id="txtcodigo">
+                    <input type="button" value="Buscar">
+                </form>
+                <div>
+                    <p>
+                        <span>Nombre:</span><br/>
+                        <span>José Pérez</span>
+                    </p>
+                    <p>
+                        <span>Edad:</span><br/>
+                        <span>12 años</span>
+                    </p>
+                </div>
             </div>
-        </div>
-        <div class="pnl_tarjeta">
-            <h4>Mi Tarjeta</h4>
-        </div>
-        <div class="pnl_menu">
-            <h4>Acciones</h4>
-            <button>Aporte</button>
-            <button>Multa</button>
-        </div>
-        <div class="pnl_lista">
-            Lista de recibos
+            <div class="pnl_tarjeta">
+                <h4>Mi Tarjeta</h4>
+            </div>
+            <div class="pnl_menu">
+                <h4>Acciones</h4>
+                <button>Aporte</button>
+                <button>Multa</button>
+            </div>
+            <div class="pnl_lista">
+                <h4>Detalles</h4>
+            </div>
         </div>
     </div>
 </body>
