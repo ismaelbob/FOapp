6eb026223f0add998d434872f4e56d616079a313
Ismael Jancko <bobismaeljg@gmail.com>
2023-08-23 15:55:10 -0400

    Corregi index, no cargaba login en Railway

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-    include 'includes/sesiones.php';
-    $sesiones = new Sesiones();
-
-    print_r($_SESSION);
-    if(isset($_SESSION["userfund"])) {
-        header('location: tarjeta.php');
-    } else {
-        header('location: vistas/login.php');
-    }
-
     // include 'includes/sesiones.php';
     // $sesiones = new Sesiones();
 
-    // $usuario = $_SESSION["userfund"];
-    
-    // if($usuario === '') {
-    //     echo '<a href="vistas/login.php">Iniciar sesion</a>';
-    // } else {
+    // print_r($_SESSION);
+    // if(isset($_SESSION["userfund"])) {
     //     header('location: tarjeta.php');
+    // } else {
+    //     header('location: vistas/login.php');
     // }
+
+    include 'includes/sesiones.php';
+    $sesiones = new Sesiones();
+
+    $usuario = $_SESSION["userfund"];
+    
+    if($usuario === '') {
+        echo '<a href="vistas/login.php">Iniciar sesion</a>';
+    } else {
+        header('location: tarjeta.php');
+    }
 ?>
